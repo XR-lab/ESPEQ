d34ace60bf6b44085495af0e58876fd3eaae904b
samjerry <jerry.sam@ziggo.nl>
2020-01-20 16:45:55 +0100

    add current content

---
+++ Assets/Scripts/SceneSwitching/NextSceneTrigger.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextSceneTrigger : MonoBehaviour {
+    
+    public void TriggerNextScene() {
+        SceneSwitcher.Instance?.GoToNextScene();
+    }
+}
