486b046058af4d001e23da2fa173a87f555cddae
samjerry <jerry.sam@ziggo.nl>
2020-01-20 17:42:44 +0100

    Added Walls, Trigger Scene switcher Script

---
+++ Assets/SwitchSceneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchSceneTrigger : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _OnTriggerEnter;
+    [SerializeField] private string _player;
+
+    private void OnTriggerEnter(Collider _other)
+    {
+        if (_other.gameObject.transform.root.name == _player)
+        {
+            _OnTriggerEnter?.Invoke();
+        }
+    }
+}
