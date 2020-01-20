09a75f0551a3c4fe95a698ed1e62100ace08f9f3
krokoloko <daan.j.ruting@gmail.com>
2020-01-20 17:26:04 +0100

    Fixed major issues except the trigger functionality

--- Assets/Scripts/Interaction/InteractionHighlight.cs
+++ Assets/Scripts/Interaction/InteractionHighlight.cs
@@ -10,6 +10,24 @@ public class InteractionHighlight : MonoBehaviour
     [SerializeField]
     private Material _selectHighlightMaterial;
 
+    [SerializeField]
+    private Material _triggerHighlightMaterial;
+
+    [SerializeField]
+    private bool _triggerAble;
+
+    [SerializeField]
+    private float _thresholdTrigger = 0.9f;
+
+    public enum Highlight { None, Hover, Selected, Triggered};
+    private enum Situation { Enter, Stay, Exit};
+
+    private Highlight _highlightState = Highlight.None;
+
+    private List<GameObject> _objectsInHover = new List<GameObject>();
+    private List<GameObject> _objectsInSelect = new List<GameObject>();
+    private List<GameObject> _objectsInTrigger = new List<GameObject>();
+
     private Renderer _rend;
 
     void Start()
@@ -17,37 +35,156 @@ public class InteractionHighlight : MonoBehaviour
         _rend = GetComponent<Renderer>();
     }
 
-    public void DisableSelectHighlight()
+    private void StateUpdate()
     {
-        RemoveMat(_selectHighlightMaterial);
-    }
-
-    public void DisableHoverHighlight()
-    {
-        RemoveMat(_hoverHighlightMaterial);   
-    }
-
-    public void EnableSelectHighlight()
-    {
-        AddMat(_selectHighlightMaterial);
-    }
-
-    public void EnableHoverHighlight()
-    {
-        AddMat(_hoverHighlightMaterial);
-    }
-
-    public void AddMat(Material mat)
-    {
-        Material[] materials = new Material[_rend.sharedMaterials.Length + 1];
-        Debug.Log(_rend.sharedMaterials.Length);
-        for(int i = 0; i < _rend.sharedMaterials.Length; i++)
+        switch (_highlightState)
         {
-            materials[i] = _rend.sharedMaterials[i];
+            case Highlight.None:
+                if (_objectsInHover.Count >= 1)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.Hover;
+                    StateAction(Situation.Enter);
+                    break;
+                }
+                StateAction(Situation.Stay);
+                break;
+            case Highlight.Hover:
+                if (_objectsInHover.Count == 0)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.None;
+                    break;
+                }
+                if (_objectsInSelect.Count >= 1)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.Selected;
+                    StateAction(Situation.Enter);
+                    break;
+                }
+                StateAction(Situation.Stay);
+                break;
+            case Highlight.Selected:
+                if (_objectsInSelect.Count == 0)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.Hover;
+                    StateAction(Situation.Enter);
+                    break;
+                }
+                
+                if (OVRInputManager.instance.GetInteract() >= _thresholdTrigger)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.Triggered;
+                    StateAction(Situation.Enter);
+                    break;
+                }
+                
+                StateAction(Situation.Stay);
+                break;
+            case Highlight.Triggered:
+                if (OVRInputManager.instance.GetInteract() < _thresholdTrigger)
+                {
+                    StateAction(Situation.Exit);
+                    _highlightState = Highlight.Selected;
+                    StateAction(Situation.Enter);
+                    break;
+                }
+                StateAction(Situation.Stay);
+                break;
         }
+    }
 
-        materials[_rend.sharedMaterials.Length] = mat;
-        _rend.sharedMaterials = materials;
+    private void StateAction(Situation state)
+    {
+        switch (_highlightState)
+        {
+            case Highlight.Hover:
+                switch (state)
+                {
+                    case Situation.Exit:
+                        RemoveMat(_hoverHighlightMaterial);
+                        break;
+                    case Situation.Enter:
+                        AddMat(_hoverHighlightMaterial);
+                        break;
+                }
+                break;
+
+            case Highlight.Selected:
+                switch (state)
+                {
+                    case Situation.Exit:
+                        RemoveMat(_selectHighlightMaterial);
+                        break;
+                    case Situation.Enter:
+                        AddMat(_selectHighlightMaterial);
+                        break;
+                }
+                break;
+
+            case Highlight.Triggered:
+                switch (state)
+                {
+                    case Situation.Enter:
+                        AddMat(_triggerHighlightMaterial);
+                        break;
+                    case Situation.Exit:
+                        RemoveMat(_triggerHighlightMaterial);
+                        break;
+                }
+                break;
+        }
+        Debug.LogError(_highlightState);
+    }
+
+    public void HandExitSelectHighlight(DetectionData data)
+    {
+        _objectsInSelect.Remove(data.Collider.gameObject);
+        StateUpdate();
+    }
+
+    public void HandExitHoverHighlight(DetectionData data)
+    {
+        _objectsInHover.Remove(data.Collider.gameObject);
+        StateUpdate();
+    }
+
+    public void HandEnterSelectHighlight(DetectionData data)
+    {
+        _objectsInSelect.Add(data.Collider.gameObject);
+        StateUpdate();
+    }
+
+    public void HandEnterHoverHighlight(DetectionData data)
+    {
+        _objectsInHover.Add(data.Collider.gameObject);
+        StateUpdate();
+    }
+
+    public void IsTriggered()
+    {
+        if (_triggerAble)
+        {
+            StateUpdate();
+        }
+    }
+    private void AddMat(Material mat)
+    {
+        int matExists = FindIndexOfMat(mat);
+        if (mat != null && matExists == -1)
+        {
+            Material[] materials = new Material[_rend.sharedMaterials.Length + 1];
+            for (int i = 0; i < _rend.sharedMaterials.Length; i++)
+            {
+                materials[i] = _rend.sharedMaterials[i];
+            }
+
+            materials[_rend.sharedMaterials.Length] = mat;
+            _rend.sharedMaterials = materials;
+        }
     }
 
     private void RemoveMat(Material mat)
@@ -56,7 +193,7 @@ public class InteractionHighlight : MonoBehaviour
         Material[] newMats = new Material[_rend.sharedMaterials.Length-1];
 
         int offset = 0;
-        if (matIndex != -1)
+        if (matIndex != -1 && mat != null)
         {
             for (int i = 0; i < _rend.sharedMaterials.Length; i++)
             {
@@ -69,7 +206,6 @@ public class InteractionHighlight : MonoBehaviour
                     offset++;
                 }
             }
-
             _rend.sharedMaterials = newMats;
         }
     }
