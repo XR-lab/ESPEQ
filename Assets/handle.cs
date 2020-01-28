f56f1d02d35c63d85ac1ca5e6b7a827c645d9158
gwnrobin <robinschellingerhout@live.nl>
2020-01-28 10:50:33 +0100

    Pulling develop

---
+++ Assets/handle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handle : MonoBehaviour
+{
+    private void OnTrigger()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger, OVRInput.Controller.Touch) < .5f)
+            return;
+
+        this.transform.position = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+    }
+    
+}
