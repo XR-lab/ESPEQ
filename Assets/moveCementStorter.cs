7268f4395cc12e406d9c0011216dcbc7cc759f43
wmartijn60 <34513939+wmartijn60@users.noreply.github.com>
2020-07-02 13:42:08 +0200

    Lever

---
+++ Assets/moveCementStorter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveCementStorter : MonoBehaviour
+{
+    public HingeJoint hinge;
+
+    void Update()
+    {
+        if (hinge.limits.min == -100)
+        {
+            
+        }
+    }
+}
