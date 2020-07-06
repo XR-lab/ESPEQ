6ca4fecfc7ded8306cfa896698058ca1db9385db
wmartijn60 <34513939+wmartijn60@users.noreply.github.com>
2020-07-06 06:54:16 +0200

    BackAndForwards movement

--- Assets/moveCementStorter.cs
+++ Assets/moveCementStorter.cs
@@ -1,23 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class moveCementStorter : MonoBehaviour
 {
     public GameObject cementBlock;
-    //public ParticleSystem cementParticle;
-    private void OnTriggerStay(Collider _coll)
+
+    public float limit1;
+    public float limit2;
+    private void OnTriggerStay(Collider coll)
     {
-        if (_coll.gameObject.CompareTag("Hand") && CompareTag("LeftButton"))
+        if (coll.gameObject.CompareTag("Hand") && CompareTag("LeftButton")  && cementBlock.transform.position.x >= limit1 )
         {
-            cementBlock.transform.Translate(Vector3.back * 0.1f);
+            cementBlock.transform.Translate(Vector3.left * -0.01f);
         }
-        if (_coll.gameObject.CompareTag("Hand") && CompareTag("RightButton"))
+        if (coll.gameObject.CompareTag("Hand") && CompareTag("RightButton") && cementBlock.transform.position.x >= limit2 )
         {
-            cementBlock.transform.Translate(Vector3.forward * 0.1f);
+            cementBlock.transform.Translate(Vector3.left * 0.01f);
         }
-    
-       
     }
 }
 
